448ccd71486303972cea340941d642ad341c6b4d
code-ry <rybussey@gmail.com>
2021-10-26 06:55:47 +1000

    First component of message randomly generated

--- message_generator_java.js
+++ message_generator_java.js
@@ -1,4 +1,9 @@
-const ryan = "legend"
-console.log(ryan)
-var createPull = 1
+const component1 = ["Cats", "Dogs", "Birds"];
+const component2 = ["Eat", "Play with","Dream of"];
+const component3 = ["Logs", "Biscuits", "Stars"];
 
+var messageArray = [];
+var randomNumber = Math.floor(Math.random() * 3)
+var message = messageArray.push(component1[randomNumber]);
+
+console.log(messageArray)
